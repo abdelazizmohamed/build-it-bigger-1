42f2d1d3a1754fe7192f6d3b90e800208bbae7cc
Dustan Curtis <dustan.curtis@gmail.com>
2015-08-31 23:24:09 -0600

    Implement layouts using material design guidelines.

--- app/src/main/java/com/dustancurtis/droid/joketeller/JokeTellerActivity.java
+++ app/src/main/java/com/dustancurtis/droid/joketeller/JokeTellerActivity.java
@@ -1,5 +1,7 @@
 package com.dustancurtis.droid.joketeller;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
